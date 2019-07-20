15a27047daef4317e91def24c0f53bae64a06f19
song <songten@icloud.com>
2019-07-21 03:02:15 +0800

    add tool

--- src/page/index/store.js
+++ src/page/index/store.js
@@ -1,13 +1,20 @@
+/* eslint-disable no-nested-ternary */
 // import createSagaMiddleware from 'redux-saga';
 import { createStore, applyMiddleware, compose } from 'redux';
+import ReduxThunk from 'redux-thunk';
 import rootReducer from './redux/gateRedux/rootReducer';
+
 // import rootSaga from './saga/index';
 // const sagaMiddleware = createSagaMiddleware();
-// let middleWare = [sagaMiddleware];
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
-  ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({})
-  : compose;
-const enhancer = composeEnhancers(applyMiddleware());
-const store = createStore(rootReducer, enhancer);
+let middleware = [ReduxThunk];
+// const store = createStore(rootReducer, applyMiddleware(ReduxThunk));
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware(...middleware)),
+);
+
 // sagaMiddleware.run(rootSaga);
 export default store;
